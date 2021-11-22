8cb797332f6f037581c545d77527e3caed7a99ab
sanket arun kale <sanketkale97.sk@gmail.com>
2021-11-22 11:01:34 +0530

    change

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
     return (
       <main>
         <div className='title'>
-          <h2>no tours left</h2>
+          <h2>No Tours Left</h2>
           <button className='btn' onClick={() => fetchTours()}>
             refresh
           </button>
